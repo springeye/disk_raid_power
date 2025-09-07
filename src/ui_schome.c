0d9aeefff53d509abf082ba3a97ca84b61565463
henjue <henjue@gmail.com>
2025-09-08 01:15:29 +0800

    init commit

--- src/ui_schome.c
+++ src/ui_schome.c
@@ -79,7 +79,7 @@ void ui_schome_screen_init(void)
     lv_obj_set_width(ui_percent, LV_SIZE_CONTENT);   /// 1
     lv_obj_set_height(ui_percent, LV_SIZE_CONTENT);    /// 1
     lv_obj_set_align(ui_percent, LV_ALIGN_CENTER);
-    lv_label_set_text(ui_percent, "78%");
+    lv_label_set_text(ui_percent, "21%");
     lv_obj_set_style_text_color(ui_percent, lv_color_hex(0xFFFFFF), LV_PART_MAIN | LV_STATE_DEFAULT);
     lv_obj_set_style_text_opa(ui_percent, 255, LV_PART_MAIN | LV_STATE_DEFAULT);
     lv_obj_set_style_text_font(ui_percent, &lv_font_montserrat_44, LV_PART_MAIN | LV_STATE_DEFAULT);
