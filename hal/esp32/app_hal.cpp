782699e6da77afcd36bdd3d6253eddabc9d1426b
chenjin <henjue@gmail.cm>
2025-09-10 11:01:34 +0800

    use TFT_eSPI

--- hal/esp32/app_hal.cpp
+++ hal/esp32/app_hal.cpp
@@ -1,23 +1,15 @@
 
 #include "app_hal.h"
 
-#include <Arduino_DataBus.h>
-#include <Arduino_GFX_Library.h>
+#include <TFT_eSPI.h>
+
 #include "hal_config.h"
 #include "lvgl.h"
 
 #define GFX_BL DF_GFX_BL // default backlight pin, you may replace DF_GFX_BL to actual backlight pin
 static const uint32_t screenWidth = SCREEN_WIDTH;
 static const uint32_t screenHeight = SCREEN_HEIGHT;
-Arduino_DataBus *bus = new Arduino_ESP32SPI(LCD_DC, LCD_CS, LCD_SCK, LCD_MOSI);
-
-/* More display class: https://github.com/moononournation/Arduino_GFX/wiki/Display-Class */
-Arduino_GFX *gfx = new Arduino_ST7789(
-  bus, LCD_RST /* RST */, 0 /* rotation */, true /* IPS */,
-  LCD_WIDTH /* width */, LCD_HEIGHT /* height */,
-  0 /* col offset 1 */, 20 /* row offset 1 */,
-  0 /* col offset 2 */, 20 /* row offset 2 */);
-
+TFT_eSPI tft = TFT_eSPI();
 
 const unsigned int lvBufferSize = screenWidth * 30;
 uint8_t lvBuffer[2][lvBufferSize];
@@ -38,7 +30,12 @@ void my_disp_flush(lv_display_t *disp, const lv_area_t *area, unsigned char *dat
 {
   uint32_t w = lv_area_get_width(area);
   uint32_t h = lv_area_get_height(area);
-  gfx->draw16bitRGBBitmap(area->x1, area->y1, (uint16_t *)data, w, h);
+
+  tft.startWrite();
+  tft.setAddrWindow(area->x1, area->y1, w, h);
+  tft.pushImageDMA(area->x1, area->y1, w, h, (uint16_t*)data);
+  tft.endWrite();
+
   lv_disp_flush_ready(disp);
 }
 
@@ -51,17 +48,15 @@ static uint32_t my_tick(void)
 void hal_setup(void)
 {
 
-  /* Initialize the display drivers */
-  // Init Display
-  if (!gfx->begin())
-  {
-    Serial.println("gfx->begin() failed!");
-  }
-  gfx->fillScreen(RGB565_BLACK);
+  tft.begin();
+  // tft.setRotation(1); // 根据你的屏幕调整方向
+  tft.fillScreen(TFT_BLACK);
 #ifdef GFX_BL
-  pinMode(GFX_BL, OUTPUT);
-  digitalWrite(GFX_BL, HIGH);
+  pinMode(LCD_BL, OUTPUT);
+  digitalWrite(LCD_BL, HIGH);
 #endif
+
+
   // lv_init();
   /* Set display rotation to landscape */
   // tft.setRotation(1);
